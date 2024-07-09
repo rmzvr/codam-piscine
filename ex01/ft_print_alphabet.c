44aeebb9a406f113d75b0781960b87c9c2b1061b
Roman Zvir <rzvir@student.codam.nl>
2024-07-09 15:28:41 +0200

    ex01

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+    char i;
+    for (i = 'a'; i <= 'z'; i++)
+    {
+        write(1, &i, 1);
+        write(1, "\n", 1);
+    }
+}
+
+int main(void)
+{
+    ft_print_alphabet();
+
+    return 0;
+}
