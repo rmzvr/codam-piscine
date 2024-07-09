faf0b5416528225476fb7d1cc8a8dce842ea7165
Roman Zvir <rzvir@student.codam.nl>
2024-07-09 16:08:04 +0200

    fix ex02

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -3,10 +3,10 @@
 void ft_print_reverse_alphabet(void)
 {
     char i;
-    for (i = 'z'; i >= 'a'; i--)
+
+    for (i = 'z'; i >= 'a'; --i)
     {
         write(1, &i, 1);
-        write(1, "\n", 1);
     }
 }
 
