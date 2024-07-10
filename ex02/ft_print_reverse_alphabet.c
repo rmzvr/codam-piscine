49a0164fe3d4afedf3e6a05566f5ad0447d54cd8
Roman Zvir <rzvir@student.codam.nl>
2024-07-10 11:22:04 +0200

    ex02

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -1,18 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 10:37:44 by rzvir             #+#    #+#             */
+/*   Updated: 2024/07/10 11:11:00 by rzvir            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_print_reverse_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
-    char i;
-
-    for (i = 'z'; i >= 'a'; --i)
-    {
-        write(1, &i, 1);
-    }
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
 }
 
-int main(void)
+int	main(void)
 {
-    ft_print_reverse_alphabet();
-
-    return 0;
+	ft_print_reverse_alphabet();
+	return (0);
 }
