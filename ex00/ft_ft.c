7ab16f5d296cd975fd7d64a6d4fe1aaa44cfff98
Roman Zvir <rzvir@student.codam.nl>
2024-07-11 16:02:08 +0200

    ex00

---
+++ ex00/ft_ft.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzvir <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 16:26:39 by rzvir             #+#    #+#             */
+/*   Updated: 2024/07/11 16:00:34 by rzvir            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+/* int	main(void)
+{
+	int	number;
+
+	number = 0;
+	printf("Number after ft_ft: %d\n", number);
+	ft_ft(&number);
+	printf("Number after ft_ft: %d\n", number);
+	return (0);
+} */
