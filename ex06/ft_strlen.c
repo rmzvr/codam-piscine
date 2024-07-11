ca05d01f60369f3cb8c42a399ff1a23680c60012
Roman Zvir <rzvir@student.codam.nl>
2024-07-11 20:02:21 +0200

    ex06

---
+++ ex06/ft_strlen.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/11 19:53:00 by rzvir             #+#    #+#             */
+/*   Updated: 2024/07/11 20:00:38 by rzvir            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/* int	main(void)
+{
+	char greeting[13]  = "Hello, World!";
+	int res;
+	res = ft_strlen(greeting);
+	printf("%d", res);
+	return (0);
+} */
