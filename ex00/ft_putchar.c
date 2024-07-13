03df2dffbbcc352b961eac0c696221aa80508e49
Roman Zvir <rzvir@student.codam.nl>
2024-07-13 11:16:59 +0200

    fix

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 10:39:12 by rzvir             #+#    #+#             */
-/*   Updated: 2024/07/10 11:42:55 by rzvir            ###   ########.fr       */
+/*   Updated: 2024/07/10 16:51:59 by rzvir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,12 @@
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+int main(void)
+{
+	char o;
+	o = '1';
+	ft_putchar(o);
+	return (0);
 }
