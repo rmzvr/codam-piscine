5851857d0e199fba6f6ffd8fc2a8d904242e9b1f
Roman Zvir <rzvir@student.codam.nl>
2024-07-19 20:13:00 +0200

    start with 0

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -6,7 +6,7 @@
 /*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 13:50:22 by rmzvr             #+#    #+#             */
-/*   Updated: 2024/07/19 17:51:03 by rzvir            ###   ########.fr       */
+/*   Updated: 2024/07/19 20:09:38 by rzvir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ int	ft_fibonacci(int index)
 {
 	if (index < 0)
 		return (-1);
-	if (index == 0 || index == 1)
+	if (index == 0)
+		return (0);
+	if (index == 1)
 		return (1);
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
 
-/* int	main(void)
+int	main(void)
 {
-	printf("%d", ft_fibonacci(12));
+	printf("%d", ft_fibonacci(3));
 	return (0);
-} */
+}
