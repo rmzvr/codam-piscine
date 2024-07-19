8fe5ab7988c093481cc8593a0eed188ccaefc119
Roman Zvir <rzvir@student.codam.nl>
2024-07-19 20:12:45 +0200

    fix power check

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: rzvir <rzvir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 13:50:22 by rmzvr             #+#    #+#             */
-/*   Updated: 2024/07/19 17:50:58 by rzvir            ###   ########.fr       */
+/*   Updated: 2024/07/19 20:08:30 by rzvir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_recursive_power(int nb, int power)
 {
-	if (nb < 0)
+	if (power < 0)
 		return (0);
 	if ((nb == 0 && power == 0) || power == 0)
 		return (1);
