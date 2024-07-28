314bb2320f6680c95df97be947055731aaad7899
Roman <rmzvr@proton.me>
2024-07-28 10:24:09 +0200

    tasks

--- ex02/ft_abs.h
+++ ex02/ft_abs.h
@@ -1,22 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rzvir <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: rmzvr <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/27 11:01:23 by rzvir             #+#    #+#             */
-/*   Updated: 2024/07/28 10:04:23 by rmzvr            ###   ########.fr       */
+/*   Created: 2024/07/28 10:05:36 by rmzvr             #+#    #+#             */
+/*   Updated: 2024/07/28 10:07:09 by rmzvr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-#define TRUE 1
-#define FALSE 0
-#define EVEN_MSG "I have an even number of arguments.\n"
-#define ODD_MSG "I have an odd number of arguments.\n"
-#define SUCCESS 0
-#define EVEN(a) (a % 2 == 0)
-
-typedef int	t_bool;
+#define ABS(Value) (Value * -1)
--- ex03/ft_point.h
+++ ex03/ft_point.h
@@ -1,22 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rzvir <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: rmzvr <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/27 11:01:23 by rzvir             #+#    #+#             */
-/*   Updated: 2024/07/28 10:04:23 by rmzvr            ###   ########.fr       */
+/*   Created: 2024/07/28 10:09:13 by rmzvr             #+#    #+#             */
+/*   Updated: 2024/07/28 10:16:26 by rmzvr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-#define TRUE 1
-#define FALSE 0
-#define EVEN_MSG "I have an even number of arguments.\n"
-#define ODD_MSG "I have an odd number of arguments.\n"
-#define SUCCESS 0
-#define EVEN(a) (a % 2 == 0)
-
-typedef int	t_bool;
+typedef struct t_point
+{
+	int	x;
+	int	y;
+}	t_point;
